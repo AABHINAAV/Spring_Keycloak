1418155bffc704fbf67c9b48cfbf4bb635cbb4e7
aabhinaav <aabhinaavgarg@gmail.com>
2023-10-21 13:28:46 +0530

    adding roles access over endpoints

--- src/main/java/com/keycloak/Learn_Keycloak/controller/EmployeeController.java
+++ src/main/java/com/keycloak/Learn_Keycloak/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
@@ -18,11 +19,14 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     @GetMapping("/getAll")
+    @RolesAllowed("admin")
     public ResponseEntity<List<Employee>> getAllEmployees() {
         return new ResponseEntity<List<Employee>>(this.employeeService.getAllEmployees(), HttpStatus.OK);
     }
 
     @GetMapping("/{empId}")
+//    @RolesAllowed("user")         // to give single role access
+    @RolesAllowed({"user", "admin"})        // multiple role access
     public ResponseEntity<Employee> getEmployee(@PathVariable("empId") int empId) {
         return new ResponseEntity<Employee>(this.employeeService.getEmployeeById(empId), HttpStatus.OK);
     }
